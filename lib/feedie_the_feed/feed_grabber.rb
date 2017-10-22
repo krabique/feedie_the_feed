61c2ca20c4730f88d49dbb9633db9d9247f21907
Jerry Wisdom <krabique48@gmail.com>
2017-10-22 14:02:34 +0000

    Remove redundant 'begin' block

--- lib/feedie_the_feed/feed_grabber.rb
+++ lib/feedie_the_feed/feed_grabber.rb
@@ -120,12 +120,10 @@ module FeedieTheFeed
     end
 
     def get_rss_feed(url)
-      begin
-        feed = Feedjira::Feed.fetch_and_parse url
-        feed.entries.map!(&:to_h)
-      rescue Feedjira::NoParserAvailable => e
-        raise BadUrl.new("The url provided doesn't seem to contain any feed", e)
-      end
+      feed = Feedjira::Feed.fetch_and_parse url
+      feed.entries.map!(&:to_h)
+    rescue Feedjira::NoParserAvailable => e
+      raise BadUrl.new("The url provided doesn't seem to contain any feed.", e)
     end
   end
 end
