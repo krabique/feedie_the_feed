2ed55aab139ad43c2d17c5be31eae53cb7c25530
Jerry Wisdom <krabique48@gmail.com>
2017-10-23 12:54:41 +0000

    Change test's fixed dynamic value to a random one

--- spec/feed_grabber.rb
+++ spec/feed_grabber.rb
@@ -7,7 +7,7 @@ describe FeedieTheFeed do
       
       it "should set @facebook_posts_limit_global to the given limit " \
         "value when we call the fb_posts_limit(limit) method" do
-        value = 38
+        value = rand(1..100)
         @feed_grabber.fb_posts_limit(value)
         output = 
           @feed_grabber.instance_variable_get(:@facebook_posts_limit_global)
