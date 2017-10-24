d48c507827c567082b07f676717b641fddc1b808
Jerry Wisdom <krabique48@gmail.com>
2017-10-24 10:31:50 +0000

    Remove a redundant block in RSpec tests, Add unimplemented tests

--- spec/feed_grabber.rb
+++ spec/feed_grabber.rb
@@ -1,18 +1,28 @@
 require 'feedie_the_feed'
 
-describe FeedieTheFeed do
-  describe FeedieTheFeed::FeedGrabber do
-    context "When testing the FeedGrabber class" do   
-      before(:each) { @feed_grabber = FeedieTheFeed::FeedGrabber.new }
-      
-      it "should set @facebook_posts_limit_global to the given limit " \
-        "value when we call the fb_posts_limit(limit) method" do
-        value = rand(1..100)
-        @feed_grabber.fb_posts_limit(value)
-        output = 
-          @feed_grabber.instance_variable_get(:@facebook_posts_limit_global)
-        expect(output).to eq value
-      end
+describe FeedieTheFeed::FeedGrabber do
+  context "When testing the FeedGrabber class" do   
+    before(:each) { @feed_grabber = FeedieTheFeed::FeedGrabber.new }
+    
+    it "should set @facebook_posts_limit_global to the given limit " \
+      "value when we call the fb_posts_limit(limit) method" do
+      value = rand(1..100)
+      @feed_grabber.fb_posts_limit(value)
+      output = 
+        @feed_grabber.instance_variable_get(:@facebook_posts_limit_global)
+      expect(output).to eq value
     end
+    
+    it "#get"
+    it "#initialize"
+    it "#reset_fb_posts_limit"
+    it "#reset_keys"
+    
+    it "exception FacebookAuthorisationError"
+    it "exception BadUrl"
+    it "exception BadFacebookPostsLimit"
+    it "exception ConnectionFailed"
+    
+    it "String truncate"
   end
 end
