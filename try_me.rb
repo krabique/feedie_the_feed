fbf3e3fe190ce6da0181ac837c529f993f2de165
Jerry Wisdom <krabique48@gmail.com>
2017-10-25 11:06:10 +0000

    Update try_me.rb

--- try_me.rb
+++ try_me.rb
@@ -1,7 +1,5 @@
 require 'feedie_the_feed'
 
-@feed_grabber = FeedieTheFeed::FeedGrabber.new
-
 links_fb =  ['https://www.facebook.com/PokerGP',
              'https://www.facebook.com/leagueoflegends/',
              'https://www.facebook.com/Defense0fTheAncients/']
@@ -12,16 +10,17 @@ links_rss = ['http://abcnews.go.com/abcnews/topstories',
 
 # Will use FACEBOOK_APPID and FACEBOOK_SECRET environment variables, if those
 # provided here are nil
-@facebook_appid = nil
-@facebook_secret = nil
-@facebook_posts_limit = nil
+options = {
+  facebook_appid:       nil,
+  facebook_secret:      nil,
+  facebook_posts_limit: nil
+}
 
-def get_feed(links)
+@feed_grabber = FeedieTheFeed::FeedGrabber.new
+
+def get_feed(links, options = {})
   links.each do |link|
-    f = @feed_grabber.get(link,
-                          facebook_posts_limit: @facebook_posts_limit,
-                          facebook_appid: @facebook_appid,
-                          facebook_secret: @facebook_secret)
+    f = @feed_grabber.get(link, options)
     p 'entry_id: '  + f.first['entry_id']
     p 'title: '     + f.first['title'] if f.first['title']
     p 'summary: '   + f.first['summary'] if f.first['summary']
@@ -32,30 +31,19 @@ def get_feed(links)
   end
 end
 
-# # Uncomment and change these accordingly if you want to provide the keys here,
-# # instead of using those from environment variables FACEBOOK_APPID and
-# # FACEBOOK_SECRET
-# @feed_grabber = FeedieTheFeed::FeedGrabber.new(
-#   facebook_appid: '123',
-#   facebook_secret: '123',
+# # Uncomment and change these accordingly if you want to provide the keys on 
+# # object creation, instead of using those from environment variables 
+# # FACEBOOK_APPID and FACEBOOK_SECRET
+# options = {
+#   facebook_appid:       '123',
+#   facebook_secret:      '123',
 #   facebook_posts_limit: 10
-# )
-#
-# def get_feed(links)
-#   links.each do |link|
-#     f = @feed_grabber.get(link)
-#     p 'entry_id: '  + f.first['entry_id']
-#     p 'title: '     + f.first['title'] if f.first['title']
-#     p 'summary: '   + f.first['summary'] if f.first['summary']
-#     p 'url: '       + f.first['url']
-#     p 'published: ' + f.first['published'].to_s if f.first['published']
-#     p 'image: '     + f.first['image'] if f.first['image']
-#     p '----------------------------------'
-#   end
-# end
+# }
+# @feed_grabber = FeedieTheFeed::FeedGrabber.new(options)
+# options = {}
 
 p ' -------- HERE GOOOOOOOOOOOOES ----- FACEBOOK --'
-get_feed links_fb
+get_feed(links_fb, options)
 
 p ' -------- HERE GOOOOOOOOOOOOES ----- RSS --'
-get_feed links_rss
+get_feed(links_rss)
