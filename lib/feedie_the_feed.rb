f662762b941ee5d5793a41d00ec504c5668b27d8
Jerry Wisdom <krabique48@gmail.com>
2017-11-02 14:50:11 +0000

    Add documentation for get class method

--- lib/feedie_the_feed.rb
+++ lib/feedie_the_feed.rb
@@ -2,6 +2,7 @@ require 'feedie_the_feed/feed_grabber'
 
 # This is the main namespace of the gem
 module FeedieTheFeed
+  # {FeedGrabber#get}
   def self.get(url, options = {})
     FeedGrabber.new.get(url, options)
   end
