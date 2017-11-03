7f9bc66b36b22f3765f07714df6b6cd2aca55a1d
Jerry Wisdom <krabique48@gmail.com>
2017-11-03 13:50:07 +0000

    Fix tests failing due to an incorrect Facebook JSON fake

--- spec/support/fake_rss.rb
+++ spec/support/fake_rss.rb
@@ -1,14 +1,14 @@
 require 'sinatra/base'
 
 class FakeRSS < Sinatra::Base
-  get '/krabique48/feedie_the_feed/master/rss_test_sample' do
-    json_response 200, 'rss_page.xml'
+  get '/rss' do
+    xml_response 200, 'rss_page.xml'
   end
 
   private
 
-  def json_response(response_code, file_name)
-    content_type :json
+  def xml_response(response_code, file_name)
+    content_type :xml
     status response_code
     File.open(File.dirname(__FILE__) + '/fixtures/' + file_name, 'rb').read
   end
